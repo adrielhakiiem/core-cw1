158aa250a8b0132374dfc33c9b3d30b38ccaf037
Adriel Hakiem <78272170+adrielhakiiem@users.noreply.github.com>
2022-12-27 04:05:00 +0800

    Question - ACF (C)
    Use the function from part b) to compute a list of approximations (𝑥𝑛) to cube root of 20000 and use pylab to plot the points 𝑥𝑛 against 𝑛 as a scatterplot and in the same plot also add a horizontal red line corresponding to the value given by `20000**(1/3)` as well as appropriate legends for the plot and labels for the 𝑥 and 𝑦 axes. 
    
    You can assume that the function `cube_root_list` is defined and works as in  part (b), in other words, the list of approximations will be given byxk_list =  cube_root_list(20000,1e-10,1)
    
    - Save the plot in a file named `outputimage.png'.
    - You can use the pre-check to view the image before submitting but the plot will be marked manually.

---
+++ ACF/ACF-C.py
@@ -0,0 +1,6 @@
+Use the function from part b) to compute a list of approximations (𝑥𝑛) to cube root of 20000 and use pylab to plot the points 𝑥𝑛 against 𝑛 as a scatterplot and in the same plot also add a horizontal red line corresponding to the value given by `20000**(1/3)` as well as appropriate legends for the plot and labels for the 𝑥 and 𝑦 axes. 
+
+You can assume that the function `cube_root_list` is defined and works as in  part (b), in other words, the list of approximations will be given byxk_list =  cube_root_list(20000,1e-10,1)
+
+- Save the plot in a file named `outputimage.png'.
+- You can use the pre-check to view the image before submitting but the plot will be marked manually. 
